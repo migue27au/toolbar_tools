5183f3de4f7825d01214a14c735a12da3e3364f7
migue27au <43569365+migue27au@users.noreply.github.com>
2025-10-27 10:51:29 -0400

    Update command path for copy-public-ip.py script

--- getPublicIP.sh
+++ getPublicIP.sh
@@ -17,7 +17,7 @@ if [ $public_ip_length -gt 3 ] && [ $public_ip_length -lt 17 ]; then
 	fi
 	echo " <txt><span weight='Bold' fgcolor='$COLOR'>$public_ip1 "
 	echo " $public_ip2 </span></txt>"
-	CMD="./copy-public-ip.py"
+	CMD="/home/kali/Documents/tools/toolbar/copy-public-ip.py"
 	printf "<txtclick>$CMD</txtclick>"
 fi
 
